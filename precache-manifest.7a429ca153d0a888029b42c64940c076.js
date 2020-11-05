86e224fa046e78964778207d0a3d34982b21cdb0
Rahul Khatri <forever_rahul16@hotmail.com>
2020-11-06 12:41:45 +1300

    Updates

---
+++ precache-manifest.7a429ca153d0a888029b42c64940c076.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a48e6e642042a64560a5d3e689d93fd",
+    "url": "/React-App-Expense-Tracker/index.html"
+  },
+  {
+    "revision": "a704915c82b1e0537b77",
+    "url": "/React-App-Expense-Tracker/static/css/main.489d9dfd.chunk.css"
+  },
+  {
+    "revision": "53d4d125a4949e33734b",
+    "url": "/React-App-Expense-Tracker/static/js/2.a7486873.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App-Expense-Tracker/static/js/2.a7486873.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a704915c82b1e0537b77",
+    "url": "/React-App-Expense-Tracker/static/js/main.b76db07b.chunk.js"
+  },
+  {
+    "revision": "35260135165ff266fe7d",
+    "url": "/React-App-Expense-Tracker/static/js/runtime-main.0f979939.js"
+  }
+]);
